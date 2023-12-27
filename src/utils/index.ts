d6da475ee54f24e1c3a7c469cf73e4844fe180f4
缪宇 <miaoyu2009@qq.com>
2023-12-27 16:11:14 +0800

    feat: pasteToEditor

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,23 @@
 import * as copyPaste from "copy-paste";
-import * as vscode from 'vscode';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as fs from "fs";
 
 export function readFileContent(filePath: string): string | undefined {
-    try {
-        // 使用同步方式读取文件内容
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-        return fileContent;
-    } catch (error) {
-        vscode.window.showErrorMessage(`Error reading file: ${error}`);
-        return undefined;
-    }
+  try {
+    // 使用同步方式读取文件内容
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    return fileContent;
+  } catch (error) {
+    vscode.window.showErrorMessage(`Error reading file: ${error}`);
+    return undefined;
+  }
 }
 
 export const getClipboardText = () => copyPaste.paste();
 
-
+export const pasteToEditor = (content: string) => {
+  const editor = vscode.window.activeTextEditor;
+  editor?.edit((builder) => {
+    builder.replace(editor.selection, content);
+  });
+};
